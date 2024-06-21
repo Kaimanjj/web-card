793a0ce60cf3116156c407515774476c0b5d9e6f
Kaimanjj <136440341+Kaimanjj@users.noreply.github.com>
2024-06-21 19:25:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-function toggle_dark_mode(){
-    let is_dark_mode = localStorage.getItem("dark_mode")
-    if (is_dark_mode=="true"){
-        is_dark_mode= false
-    }
-    else{
-        is_dark_mode= true
-    }
-    localStorage.setItem("dark_mode",is_dark_mode)
-    document.querySelector("header").classList.toggle("dark-mode",is_dark_mode)
-    document.querySelector("main").classList.toggle("main-dark-mode",is_dark_mode)
-    document.querySelector("footer").classList.toggle("dark-mode",is_dark_mode)
+AOS.init();
 
-    document.querySelectorAll(".color-background").forEach(element => {
-        element.classList.toggle("main-dark-mode",is_dark_mode)
-    });
+function toggle_dark_mode() {
+  let is_dark_mode = localStorage.getItem("dark_mode");
+  if (is_dark_mode == "true") {
+    is_dark_mode = false;
+  } else {
+    is_dark_mode = true;
+  }
+  localStorage.setItem("dark_mode", is_dark_mode);
+  document.querySelector("header").classList.toggle("dark-mode", is_dark_mode);
+  document
+    .querySelector("main")
+    .classList.toggle("main-dark-mode", is_dark_mode);
+  document.querySelector("footer").classList.toggle("dark-mode", is_dark_mode);
+
+  document.querySelectorAll(".color-background").forEach((element) => {
+    element.classList.toggle("main-dark-mode", is_dark_mode);
+  });
 }
 
-
-let button = document.querySelector(".knopka")
-button.addEventListener("click",toggle_dark_mode)
+let button = document.querySelector(".knopka");
+button.addEventListener("click", toggle_dark_mode);
